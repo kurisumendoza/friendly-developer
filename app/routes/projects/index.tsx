a940520c21ce441ed99bdb04d73ba93ba3086afb
Kristopher Mendoza <creative.kristopher@gmail.com>
2025-12-18 03:20:12 +0800

    feat: add loader with custom type for fetching projects

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,4 +1,19 @@
-const ProjectsPage = () => {
+import type { Route } from './+types/index';
+import type { Project } from '~/types';
+
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+  const res = await fetch('http://localhost:8000/projects');
+  const data = await res.json();
+
+  return { projects: data };
+}
+
+const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData as { projects: Project[] };
+  console.log(projects);
+
   return (
     <>
       <h2 className="text-3xl text-white font-bold mb-8">🚀 Projects</h2>
