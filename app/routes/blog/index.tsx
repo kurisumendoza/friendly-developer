4cd7787aab480f6519345e3d684217538a9288f6
Kristopher Mendoza <creative.kristopher@gmail.com>
2025-12-20 04:02:26 +0800

    feat: add blog data and type

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,4 +1,23 @@
-const BlogPage = () => {
+import type { Route } from './+types/index';
+import type { PostMeta } from '~/types';
+import { Link } from 'react-router';
+
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
+  const url = new URL('/posts-meta.json', request.url);
+  const res = await fetch(url.href);
+
+  if (!res.ok) throw new Error('Failed to fetch data');
+
+  const data = await res.json();
+
+  return { posts: data };
+}
+
+const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const { posts } = loaderData;
+
   return (
     <>
       <h2 className="text-3xl text-white font-bold mb-8">📝 Blog</h2>
