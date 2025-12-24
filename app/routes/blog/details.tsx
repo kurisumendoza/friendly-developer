c30f99ce18db3cacd6f86b42df5545249f95ad37
Kristopher Mendoza <creative.kristopher@gmail.com>
2025-12-25 02:30:54 +0800

    feat: update image urls

--- app/routes/blog/details.tsx
+++ app/routes/blog/details.tsx
@@ -25,9 +25,7 @@ export async function loader({ request, params }: Route.LoaderArgs) {
     excerpt: item.excerpt,
     date: item.date,
     body: item.body,
-    image: item.image?.url
-      ? `${import.meta.env.VITE_STRAPI_URL}${item.image.url}`
-      : '/images/no-image.png',
+    image: item.image?.url ? `${item.image.url}` : '/images/no-image.png',
   };
 
   return { posts };
